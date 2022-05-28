60eb0a3a6060b32aa6f51f59d43feb277e1e6103
NehadAwad <nehadakash622@gmail.com>
2022-05-28 23:53:38 +0600

    getAllProductsStatic function added in controller

---
+++ controllers/products.js
@@ -0,0 +1,8 @@
+const Product = require('../models/product');
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({ price: { $gt: 30 } })
+      .sort('price')
+      .select('name price');
+  
+    res.status(200).json({ products, nbHits: products.length });
+  };
