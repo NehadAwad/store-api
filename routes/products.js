bb2da099ea6f04e33c2ac06b8c0839af714c97b9
NehadAwad <nehadakash622@gmail.com>
2022-06-05 22:56:46 +0600

    routes added

--- routes/products.js
+++ routes/products.js
@@ -6,6 +6,8 @@ const {
   getAllProductsStatic,
 } = require('../controllers/products')
 
+router.route('/').get(getAllProducts)
+router.route('/static').get(getAllProductsStatic)
 
 
 module.exports = router
