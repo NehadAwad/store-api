00139dc52549fb56695e5c8361de71845033d91b
NehadAwad <nehadakash622@gmail.com>
2022-06-09 22:29:38 +0600

    product model added

--- populate.js
+++ populate.js
@@ -8,7 +8,6 @@ const jsonProducts = require('./products.json')
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
-    await Product.deleteMany()
     await Product.create(jsonProducts)
     console.log('Success!!!!')
     process.exit(0)
